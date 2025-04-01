952b7632555cfe0e4e4c24134f65472742f95be6
anna leme <annaleme.tds1.senai@gmail.com>
2025-04-01 18:51:03 -0300

    refactor: syntax

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './src/screens/HomeScreen';
-import InputScreen from './src/screens/InputScreen';
+import HomeScreen from './src/screen/HomeScreen';
+import InputScreen from './src/screen/InputScreen';
 
 const Stack = createStackNavigator();
 
